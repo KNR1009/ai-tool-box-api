4af526318ae32cf25926b7bc8903936580d58085
伊東和成 <itokazunari5375@icloud.com>
2023-08-21 12:53:24 +0900

    feat: api用のコードを少し変更

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
-import streamlit as st
-from streamlit_chat import message
+from fastapi import FastAPI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
@@ -7,52 +6,23 @@ from langchain.schema import (
     AIMessage
 )
 from langchain.callbacks import get_openai_callback
-
-import requests
+from pydantic import BaseModel
 from bs4 import BeautifulSoup
+import requests
 from urllib.parse import urlparse
-
 import os
 from dotenv import load_dotenv
 
+app = FastAPI()
 
-# .envファイルから環境変数を読み込む
 load_dotenv()
-
-# 環境変数からOpenAIのAPIキーを取得する
 openai_api_key = os.getenv("OPENAI_API_KEY")
-
-def init_page():
-    st.set_page_config(
-        page_title="Website Summarizer",
-        page_icon="🤗"
-    )
-    st.header("Website Summarizer 🤗")
-    st.sidebar.title("Options")
+model_name = "gpt-3.5-turbo"  # または "gpt-3.5-turbo"
+llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0, model_name=model_name)
 
 
-def init_messages():
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-    if clear_button or "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-        st.session_state.costs = []
-
-
-def select_model():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-    if model == "GPT-3.5":
-        model_name = "gpt-3.5-turbo"
-    else:
-        model_name = "gpt-4"
-
-    return ChatOpenAI(openai_api_key=openai_api_key, temperature=0, model_name=model_name)
-
-
-def get_url_input():
-    url = st.text_input("URL: ", key="input")
-    return url
+class UrlQuery(BaseModel):
+    url: str
 
 
 def validate_url(url):
@@ -65,27 +35,30 @@ def validate_url(url):
 
 def get_content(url):
     try:
-        with st.spinner("Fetching Content ..."):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # fetch text from main (change the below code to filter page)
-            if soup.main:
-                return soup.main.get_text()
-            elif soup.article:
-                return soup.article.get_text()
-            else:
-                return soup.body.get_text()
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if soup.main:
+            return soup.main.get_text()
+        elif soup.article:
+            return soup.article.get_text()
+        else:
+            return soup.body.get_text()
     except:
-        st.write('something wrong')
         return None
 
 
 def build_prompt(content, n_chars=300):
-    return f"""以下はとある。Webページのコンテンツである。内容を「## ツール名」「## 特徴」「## 使用例」「## 対象者(箇条書き)」「## ツールの説明」の見出しでそれぞれまとめてください
+    return f"""以下はとあるWebページのコンテンツである。内容を以下の見出しでそれぞれまとめてください。
+
+- ## ツール名
+- ## 特徴
+- ## 使用例
+- ## 対象者(箇条書き)
+- ## ツールの説明
 
 ========
 
-{content[:1000]}
+{content[:n_chars]}
 
 ========
 
@@ -93,51 +66,22 @@ def build_prompt(content, n_chars=300):
 """
 
 
-def get_answer(llm, messages):
-    with get_openai_callback() as cb:
-        answer = llm(messages)
-    return answer.content, cb.total_cost
+@app.post("/summarize")
+def summarize(query: UrlQuery):
+    url = query.url
+    is_valid_url = validate_url(url)
 
+    if not is_valid_url:
+        return {"error": "Please input valid url"}
 
-def main():
-    init_page()
+    content = get_content(url)
+    if content:
+        prompt = build_prompt(content)
+        messages = [SystemMessage(content="You are a helpful assistant."), HumanMessage(content=prompt)]
 
-    llm = select_model()
-    init_messages()
-
-    container = st.container()
-    response_container = st.container()
-
-    with container:
-        url = get_url_input()
-        is_valid_url = validate_url(url)
-        if not is_valid_url:
-            st.write('Please input valid url')
-            answer = None
-        else:
-            content = get_content(url)
-            if content:
-                prompt = build_prompt(content)
-                st.session_state.messages.append(HumanMessage(content=prompt))
-                with st.spinner("ChatGPT is typing ..."):
-                    answer, cost = get_answer(llm, st.session_state.messages)
-                st.session_state.costs.append(cost)
-            else:
-                answer = None
-
-    if answer:
-        with response_container:
-            st.markdown("## Summary")
-            st.write(answer)
-            st.markdown("---")
-            st.markdown("## Original Text")
-            st.write(content)
-
-    costs = st.session_state.get('costs', [])
-    st.sidebar.markdown("## Costs")
-    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
-    for cost in costs:
-        st.sidebar.markdown(f"- ${cost:.5f}")
-
-if __name__ == '__main__':
-    main()
+        with get_openai_callback() as cb:
+            answer = llm(messages)
+        
+        return {"summary": answer.content}
+    else:
+        return {"error": "something went wrong"}
