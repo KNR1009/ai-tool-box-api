10a511f2a785ee51b02df4984fd8fe46aafcbff3
伊東和成 <itokazunari5375@icloud.com>
2023-08-27 09:51:49 +0900

    feat: htmlからドキュメントを取得

--- url_loader.py
+++ url_loader.py
@@ -1,21 +1,15 @@
-from fastapi import FastAPI
-from playwright.async_api import async_playwright
+from langchain.document_loaders import SeleniumURLLoader
 
-app = FastAPI()
+urls = [
+    "https://udemy.benesse.co.jp/development/langchain.html",
+]
 
-async def get_data_from_url():
-    urls = [
-        "https://www.youtube.com/watch?v=dzXZg7Ki7AY",
-    ]
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto(urls[0])
-        content = await page.content()
-        await browser.close()
-        return content
+loader = SeleniumURLLoader(urls=urls)
 
-@app.get("/")
-async def read_root():
-    data = await get_data_from_url()
-    return {"data": data}
+data = loader.load()
+
+print("================")
+print(data)
+print("================")
+
+
